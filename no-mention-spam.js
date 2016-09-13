9f3e582cecdf0df2b2295e4d492fde79309ab4cd
Evelyne Lachance <eslachance@gmail.com>
2016-09-13 09:37:39 -0400

    Resolved most obvious bugs, this is a stable release

--- no-mention-spam.js
+++ no-mention-spam.js
@@ -22,16 +22,26 @@ bot.on('guildCreate', (guild) => {
   console.log(`New guild has been joined: ${guild.name}`);
   confs.add(guild).then(console.log).catch(console.error);
   let server_owner = guild.owner;
-  server_owner.sendMessage(`Hi ${server_owner.username}! Sorry to bother you, but I kinda have to.
+  server_owner.sendMessage(`Hi ${server_owner.user.username}! Sorry to bother you, but I kinda have to.
 I'm a bot, see, that can only be configured, initially, by the server owner, to prevent tampering.
-Please use \`spambot.help\` to get a list of owner/mod commands. YOU are the only one who can set the \`mod_role\` configuration, which *has* to be an ID.
+Please use \`spambot.help\` to get a list of owner/mod commands. YOU are the only one who can set the \`mod_role\` configuration (name or id of role)
 Anyone with the role will be able to set the rest of the options.
 Again, this secures and simplifies the process and I'm sorry to bother you!`);
 });
 
+const banned_ids = ["224418473286041601", "224426434339274752", "203868728884985857", "224431797533016064"];
+bot.on("guildMemberAdd", (guild, member) => {
+  console.log(`${member.user.username} ("${member.user.id}") joined ${guild.name}`);
+  if(banned_ids.includes(member.user.id)) {
+    member.ban().then( () =>{
+      console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] Pre-Emptively Banned ${member.user.username} from ${guild.name}`);
+    }).catch(console.error);
+  }
+})
+
 bot.on('guildDelete', (guild) => {
-  console.log(`I've been removed from: ${guild.name}`);
-  //confs.add(guild);
+  console.log(`[GUILD DELETED]: I've been removed from: ${guild.name}`);
+  confs.remove(guild).catch(console.error);
 });
 
 bot.on('message', message => {
@@ -41,21 +51,28 @@ bot.on('message', message => {
   
 	var conf = confs.get("default");
 	if(message.guild) {
-	  conf = confs.get(message.guild.id);
+	  conf = confs.get(message.guild);
+	}
+	
+	// Temporary (?) exception to this fucking asshole?
+	if(message.content.includes("👎Hillary👎")) {
+	  message.guild.member(message.author).ban(1).then(() => {
+	    console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] Banned ${message.author.username} from ${message.guild.name}`);
+	  });
 	}
 
-  if(message.mentions.users.size > parseInt(conf.ban_level, 10)) {
-    
+  if(parseInt(conf.ban_level, 10) > 0 && message.mentions.users.size > parseInt(conf.ban_level, 10)) {
     // Add to ban log
+    /*
     db.open('./modlog.sqlite').then(() => {
       db.run(`INSERT INTO "banlog" (user_id, username, user_dump, mention_count, message_content, server_id, server_name, channel_id, channel_name) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`, 
       [message.author.id, message.author.username, util.inspect(message.author), message.mentions.users.size, message.content, message.guild.id, message.guild.name, message.channel.id, message.channel.name]).then ( ()=> {
         console.log(`done`);
       }).catch(console.error);
-    });
+    });*/
     
     // Add to Discord Global Ban list
-    if(settings.dbots.url) post_global_ban(message);
+    //if(settings.dbots.url) post_global_ban(message);
     
     message.guild.member(message.author).ban(1).then(() => {
       message.channel.sendMessage(`:no_entry_sign: User ${message.author.username} has just been banned for mentionning too many users. :hammer: 
@@ -65,27 +82,21 @@ bot.on('message', message => {
     return;
   }
 
-  if(message.mentions.users.size > parseInt(conf.kick_level, 10)) {
+  if(parseInt(conf.kick_level, 10) > 0 && message.mentions.users.size > parseInt(conf.kick_level, 10)) {
     let kick_msg = `${message.author.username} has been kicked for using too many mentions.`;
-    message.member.kick().then(() => {
+    message.member.ban().then(mem => message.guild.unban(mem.user)).then(() => {
       if(conf.modlog_channel) {
         message.guild.channels.get(conf.modlog_channel).sendMessage(kick_msg)
         .catch(console.error);
       } else {
-        message.channel.sendMessage(kick_msg)
-        .catch(console.error);
+        //message.channel.sendMessage(kick_msg)
+        //.catch(console.error);
       }
     })
     .catch(console.error);
+    return;
   }
 
-  if(message.mentions.users.size > parseInt(conf.warn_level, 10)) {
-    let warn_message = `:warning: ${message.author.username}`, warning 
-    message.channel.sendMessage(`${message.author.username}, don't mention too many people at once please.`);
-  }
-
-  
-  
 });
 
 bot.on('message', message => {
@@ -93,24 +104,31 @@ bot.on('message', message => {
 
 	var conf = confs.get("default");
 	if(message.guild) {
-	  conf = confs.get(message.guild.id);
+	  conf = confs.get(message.guild);
+    //console.log(`<${message.guild.name}> #${message.channel.name} ; ${message.author.username}:  ${message.content}`);
 	}
+	
+  if(!message.content.startsWith(conf.prefix) && !message.content.startsWith(settings.default_server_conf.prefix)) return;
   
-  if(!message.content.startsWith(conf.prefix)) return;
-  
+  let command = message.content.split(" ")[0];
+  if(command.startsWith(conf.prefix)) command = command.replace(conf.prefix, "");
+  if(command.startsWith(settings.default_server_conf.prefix)) command = command.replace(settings.default_server_conf.prefix, "");
   var params = message.content.split(" ");
   
-  if(message.content === conf.prefix + "info") {
+  if(command === "info") {
     return message.channel.sendMessage(`Hi ${message.author.username}!
     I'm no-mention-spam and I protect you against mention spams!
-    To invite me to your server click here: http://tiny.cc/NOMentionspambot
-    I am currently on ${bot.guilds.size} servers, kicking spammers' asses to hell and back!
-    I was created by LuckyEvie#4611 (139412744439988224)`);
+    I am currently on ${bot.guilds.size} servers, and was created by LuckyEvie#4611 (139412744439988224)`);
+  }
+  
+  if(command === "invite") {
+    return message.channel.sendMessage(`To invite this bot:
+    <https://discordapp.com/oauth2/authorize?client_id=219487222913695754&scope=bot&permissions=4>
+    For support, join https://discord.gg/7x4JmsH !`)
   }
   
   // eh, I have to put this before server check so I can do private evals. So sue me.
-  if(message.author.id === settings.ownerid && message.content.startsWith(conf.prefix + "eval")) {
-    //let 
+  if(message.author.id === settings.ownerid && command === "eval") {
     try {
       var suffix = params.splice(1).join(" ");
       var evaled = eval(suffix);
@@ -132,17 +150,21 @@ bot.on('message', message => {
 
   // check perms
   // MOD Commands
+  try{ 
+  //console.log(conf.mod_role);
   let mod_role = conf.mod_role ? conf.mod_role : null;
   let server_owner = message.guild.owner.id;
   var perm_level = 0;
-  if(mod_role && message.member.roles.exists("id", mod_role.id)) perm_level = 1;
-  if(message.author.id === "68396159596503040") perm_level = 1;
+  if(mod_role && message.member.roles.exists("id", mod_role)) perm_level = 1;
+  if(message.author.id === "68396159596503040") perm_level = 1; // I see you, Carbonitex (Matt).
   if(message.author.id === server_owner) perm_level = 2;
   if(message.author.id === settings.ownerid) perm_level = 3;
-  
+  } catch (e) {
+    console.error(e);
+  }
   console.log(`Command ${params[0]} called by ${message.author.username}(${message.author.id}) in ${message.guild.name}:\n  ${message.content}`);
 
-  if(message.content === conf.prefix + "help") {
+  if(command === "help") {
     message.author.sendMessage(help_message);
     if(message.guild) message.reply(`Please check Direct Messages for help!`);
     return;
@@ -150,24 +172,35 @@ bot.on('message', message => {
 
   if(perm_level < 1) return;
   // MOD_ROLE COMMANDS
-  if(message.content.startsWith(conf.prefix + "conf get")) {
+  //console.log(params);
+  let conf_cmd = "";
+  if(["get", "set", "view"].includes(params[1])) conf_cmd = params[1];
+  //console.log(conf_cmd);
+  
+  if(command == "conf" && conf_cmd == "get") {
     if(!conf[params[2]]) return message.reply(`Key \`${params[2]}\` not found in server configuration.`);
     return message.reply(`Configuration key \`${params[2]}\` currently set to \`${conf[params[2]]}\``);
   }
   
-  if(message.content.startsWith(conf.prefix + "conf set")) {
+  if(command == "conf" && conf_cmd == "set") {
     if(params[2] === "mod_role" && perm_level < 2) return message.reply(`\`${params[2]}\` can *only* be set by the server owner!`);
-    confs.set(message.guild, params[2], params[3])
+    confs.set(message.guild, params[2], params.slice(3).join(" "))
     .then((e) => message.reply(e))
-    .catch((e) => message.reply(e));
+    .catch((e) => {
+      message.reply(e);
+      console.error(e);
+    });
   }
   
-  if(message.content.startsWith(conf.prefix + "conf view")) {
-    return message.channel.sendMessage(make_conf(conf));
+  if(command == "conf" && conf_cmd == "view") {
+    return message.channel.sendMessage(make_conf(conf, message));
   }
-
   
 });
+
+bot.on('error', (error) => {
+  console.error(error);
+})
 
 bot.login(settings.token);
 
@@ -206,36 +239,48 @@ spambot.conf - Configure the server settings
     'modifies the value for your server.'
   conf view
     'displays the current server configuration.'
+    
+Example:
+  spambot.conf set ban_level 10
+  spambot.conf set prefix >
+  >invite
+  (because prefix changed)
 
 Available configuration keys: 
   prefix - <String> (false to reset)
     'custom server prefix. Disables the default "spambot." prefix.'
-  warn_level - <Int> (0 to disable, Default 5)
-    'mention count at which the bot warns the user of mentions.'
   kick_level - <Int> (0 to disable, Default 10)
     'mention count at which to kick the user from the server.'
   ban_level - <Int> (min 2, Default 15)
     'mention count at which to ban the user. Removes 1 day of messages.'
-  mod_role - <RoleID> (false to disable)
-    'the ID of the role that has permission to change config besides the server owner.'
+  mod_role - <RoleName> (false to disable)
+    'the name of the role that has permission to change config besides the server owner.'
   modlog_channel - <ChannelID>
     'the ID of the channel where mod logs should be posted (warn/kick/ban)'
   get_global_bans - <Boolean> (true/false)
     'whether to retrieve Dbans global bans for known mention spammers.'
 \`\`\``;
 
-function make_conf(conf) {
+function make_conf(conf, message) {
   return `\`\`\`xl
 CURRENT SERVER CONFIGURATION
 This is your current server configuration.
-Name: ${conf.server_name}
-Owner: ${bot.guilds.get(conf.server_id).owner.username}
+Name: ${message.guild.name}
+Owner: ${message.guild.owner.user.username}
 
-prefix        : ${conf.prefix}
-warn_level    : ${conf.warn_level}
-kick_level    : ${conf.kick_level}
-ban_level     : ${conf.ban_level}
-mod_role      : ${conf.mod_role}
-modlog_channel: ${conf.modlog_channel}
+prefix         : ${conf.prefix}
+kick_level     : ${conf.kick_level}
+ban_level      : ${conf.ban_level}
+mod_role       : ${conf.mod_role}
+modlog_channel : ${conf.modlog_channel}
+get_global_bans: ${conf.get_global_bans}
 \`\`\``;
 }
+
+process.on('uncaughtException', (err) => {
+  let errorMsg = err.stack.replace(new RegExp(`${__dirname}\/`, 'g'), './');
+  // bot.getDMChannel('175008284263186437').then(DMChannel => {
+  //   bot.createMessage(DMChannel.id, `\`UNCAUGHT EXCEPTION\`\n\`\`\`sh\n${errorMsg}\n\`\`\``);
+  // }).catch(error);
+ console.error(errorMsg);
+});
--- serverconf.js
+++ serverconf.js
@@ -7,23 +7,31 @@ const default_conf = require('./auth.json').default_server_conf;
 
 exports.init = (bot) => {
   db.open('./modlog.sqlite').then(() => db.all('SELECT * FROM server_configs')).then( rows => {
-    for(const row of rows) serverconf.set(row.server_id, row);
-    for(const guild of bot.guilds) {
+    require('fs').writeFile('./dump.txt', require('util').inspect(rows));
+    for(const row of rows) {
+      serverconf.set(row.server_id, row);
+    }
+    bot.guilds.forEach(guild => {
       if(!serverconf.has(guild.id)) {
         this.add(guild).then( conf => {
-          serverconf.set(guild.id, conf);
-        });
+          // nope.
+        }).catch(console.error);
       }
-    }
+    });
   }).catch(console.error);
 };
 
 exports.add = (server) => {
   return new Promise( (resolve, reject) => {
     if(serverconf.has(server.id)) return reject(`Server is already in the configurations`);
+    //console.log(`INSERT INTO "server_configs" (server_id, server_name) VALUES ('${server.id}', '${server.name}')`)
     db.open('./modlog.sqlite').then(() => {
-      db.run(`INSERT INTO "server_configs" (server_id, server_name) VALUES (?, ?)`, [server.id, server.name]).then ( ()=> {
-        resolve({server_id: server.id, server_name: server.name});
+      db.run(`INSERT INTO "server_configs" (server_id, server_name) VALUES ('${server.id}', '${server.name}')`).then ( () => {
+        let conf = default_conf;
+        conf.server_id = server.id;
+        conf.server_name = server.name;
+        serverconf.set(server.id, conf);
+        resolve();
       }).catch(reject);
     });
   });
@@ -33,9 +41,12 @@ exports.remove = (server) => {
   return new Promise( (resolve, reject) => {
     if(!serverconf.has(server.id)) return reject();
     db.open('./modlog.sqlite').then( () => {
-      db.run(`DELETE FROM "server_configs" WHERE server_id = ?`, [server.id]).then( () => {
-        resolve();
-      }).catch(reject);
+      db.run(`DELETE FROM "server_configs" WHERE server_id = ?`, [server.id])
+        .then(() => {
+          serverconf.delete(server.id);
+          resolve();
+        })
+        .catch(reject);
     });
   });
 };
@@ -44,9 +55,9 @@ exports.has = (serverid) => {
   return serverconf.has(serverid);
 };
 
-exports.get = (serverid) => {
-  if(serverconf.has(serverid)) {
-    let server_conf = serverconf.get(serverid);
+exports.get = (server) => {
+  if(serverconf.has(server.id)) {
+    let server_conf = serverconf.get(server.id);
   	const conf = {};
   	if(server_conf) {
       for(let key in server_conf) {
@@ -56,6 +67,7 @@ exports.get = (serverid) => {
           conf[key] = default_conf[key];
         }
       }
+      conf["default"] = false;
   	}
   	return conf;
   }
@@ -70,7 +82,7 @@ exports.set = (server, key, value) => {
     key = key.replace(/[';]]/g, '');
     
     let serverid = server.id;
-    if(!serverconf.has(serverid)) {
+    if(!serverconf.has(server.id)) {
      reject(`:x: The server ${serverid} not found while trying to set ${key} to ${value}`);
      return;
     }
@@ -81,25 +93,43 @@ exports.set = (server, key, value) => {
      return;
     }
 
-    if(["server_id", "server_name"].includes(key)) reject(`:x: The key \`${key}\` is read-only.`);
-    if(key === "ban_level" && parseInt(value, 10) < 3) reject(`:x: The key \`ban_level\` must be higher than 2.`);
+    if(["server_id", "server_name"].includes(key)) return reject(`:x: The key \`${key}\` is read-only.`);
+    if(key === "ban_level" && parseInt(value, 10) < 3) return reject(`:x: The key \`ban_level\` must be higher than 2.`);
     if(["ban_level", "kick_level", "warn_level"].includes(key)) {
-      value = parseInt(value, 10);
-      if(value < 1) reject(`:x: The key \`${key}\` value must be numeric.`);
+      if(isNaN(value)) { 
+        return reject(`:x: The key \`${key}\` value must be numeric.`);
+      } else if(parseInt(value) < 1) {
+        value = "false";
+      } else {
+        value = parseInt(value);
+      }
     }
     if(key === "get_global_bans") {
-      value = JSON.parse(value);
-      if(!typeof value === "boolean") reject(`:x: The key \`${key}\` value must resolve to boolean.`);
+      if(!typeof JSON.parse(value) === "boolean") return reject(`:x: The key \`${key}\` value must resolve to boolean.`);
     }
     if(key === "mod_role") {
-      if(!server.roles.exists("id", value)) reject(`:x: The key \`${key}\` must be a correct Role ID for this server.`);
+      const role = server.roles.exists("name", String(value));
+      const role_by_id = server.roles.exists("id", String(value));
+      if(role) {
+        console.log(`Found role by name!`);
+        value = server.roles.find("name", value).id;
+      } else
+      if(role_by_id) {
+        console.log(`Found role by ID!`);
+        value = value; // ¯\_(ツ)_/¯
+      } else {
+        console.error(`Role not found`);
+        return reject(`:x: Could not find the specified role. Note: roles are case-sensitive`);
+      }
     }
     
+    //console.log(`Trying to set ${key} to ${value} in : \n${JSON.stringify(thisconf)}`)
     thisconf[key] = value;
     serverconf.set(serverid, thisconf);
     
     if(isNaN(value)) value = `'${value}'`;
     let query = `UPDATE "server_configs" SET ${key} = ${value} WHERE server_id = '${serverid}'`;
+
     //console.log(query);
     
     db.open('./modlog.sqlite').then(() => {
@@ -113,3 +143,11 @@ exports.set = (server, key, value) => {
   });
 };
 //{$key: key, $value: value, $serverid: serverid}
+
+process.on('uncaughtException', (err) => {
+  let errorMsg = err.stack.replace(new RegExp(`${__dirname}\/`, 'g'), './');
+  // bot.getDMChannel('175008284263186437').then(DMChannel => {
+  //   bot.createMessage(DMChannel.id, `\`UNCAUGHT EXCEPTION\`\n\`\`\`sh\n${errorMsg}\n\`\`\``);
+  // }).catch(error);
+ console.error(errorMsg);
+});
